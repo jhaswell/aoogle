b31889c157b0bd4739f12fb605e301691e6adc0c
Josiah Haswell <jhaswell@electric-cloud.com>
2013-02-21 22:38:20 -0700

    Implemented SVD

--- lsa/src/LSA.hs
+++ lsa/src/LSA.hs
@@ -1,4 +1,13 @@
 
-main = do 
-	putStrLn "Hello, world!"
+termFrequency document term = length (filter (==t) document)
+
+compress k m = u_k <> sigma_k <> v_k where
+	(u,sigma,v) = fullSVD m			       
+	sigma_k = (takeColumns k . takeRows k) sigma	
+	u_k = takeColumns k u				
+	v_k = takeRows k $ trans v			
+
+collapseDimensions k m = v_k where
+        (u,sigma,v) = fullSVD m                        
+        v_k = takeRows k $ trans v                      
 
